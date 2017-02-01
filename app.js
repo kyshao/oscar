3a92154834a3b8895ea8d3a6fd29db091ab62364
Sandbox User <oscarseng371@gmail.com>
2017-01-31 17:45:38 -0800

    modularity

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var mysql = require("mysql");
-var fs = require('fs');
+//global consts here
+const mysql = require("mysql");
+const fs = require('fs');
+const oscarCredentials = {  host: "localhost",  user: "root",  password: "@scar2015"}
+
+//execution here
 
 // First you need to create a connection to the db
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "@scar2015"
-});
+var con = mysql.createConnection(oscarCredentials);
 
 con.connect(function(err){
   if(err){
@@ -16,40 +16,55 @@ con.connect(function(err){
   console.log('Connection established');
 });
 
-con.query('show databases',function(err,rows){
-  if(err) throw err;
-
-  //Async processing sucks
-  console.log('Please make sure that output.txt does not already exist before running this program');
-
-
-  console.log('Data received from Db:\n');
-  console.log(rows);
-
-  //output to file
-  //if the stream is declared outside the function it is not within scope
-  var stream = fs.createWriteStream("output.txt", {flags:'a'});
-  stream.once('open', function(fd) {
-    stream.write(JSON.stringify(rows));//todo end with a newline
-    stream.write("END stream1\n");
-    stream.end();
-  });
-  var stream2 = fs.createWriteStream("output.txt", {flags:'a'});
-  stream2.once('open', function(fd) {
-    stream2.write("placeholder for stream2 output\n");
-    stream2.write("END stream2\n");
-    stream2.end();
-  });
-
-  console.log('File output complete\n');
-
-});
-
+queryDB(con, "SHOW DATABASES", "console");
+queryDB(con, "USE oscar15_bc", "console");
+var cols = queryDB(con, "SHOW COLUMNS FROM AppDefinition", "output.txt");
+console.log("cols"+JSON.stringify(cols));
 
 con.end(function(err) {
   // The connection is terminated gracefully
   // Ensures all previously enqueued queries are still
   // before sending a COM_QUIT packet to the MySQL server.
 });
+
+
+
+//=====================
+//functions here
+
+function queryDB(con, command, output){
+	var r;
+	con.query(command,function(err,rows){
+	  if(err) throw err;
+
+	  //Async processing sucks
+	  //console.log('Please make sure that output.txt does not already exist before running this program');
+
+	  if(output == "console"){
+		  console.log('Data received from Db:\n');
+		  console.log(rows);
+	  }
+	  else if(output != undefined){
+
+		  //output to file
+		  //if the stream is declared outside the function it is not within scope
+		  var stream = fs.createWriteStream(output, {flags:'a'});
+		  stream.once('open', function(fd) {
+		    stream.write(JSON.stringify(rows)+'\n');
+		    stream.write("END stream1\n");
+		    stream.end();	
+		  });
+		  var stream2 = fs.createWriteStream(output, {flags:'a'});
+		  stream2.once('open', function(fd) {
+		    stream2.write("placeholder for stream2 output\n");
+		    stream2.write("END stream2\n");
+		    stream2.end();
+		  });
+	  console.log('File output complete\n');
+	}
+	r=rows;//todo this does not work
+	});
+	return r;
+}
 
 
